7f6ebb9c09daf3a9053d70176adc87d3b6aa0a11
Semen <semenkaurin@gmail.com>
2023-12-12 01:02:51 +0300

    сру

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from kubernetes import client, config
 
 config.load_kube_config()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+api = client.CoreV1Api()
+
+for item in api.list_node().items:
+    node = api.read_node(name=item.metadata.name)
+    print(node)
