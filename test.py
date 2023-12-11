30502c41d5e5cd05592cbf8bb0970908afdf67a7
Semen <semenkaurin@gmail.com>
2023-12-12 01:28:02 +0300

    сру

--- test.py
+++ test.py
@@ -9,8 +9,7 @@ def main():
 
     v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
+    #ret = v1.list_pod_for_all_namespaces(watch=False)
+    ret = v1.list_component_status()
     for i in ret.items:
-
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
