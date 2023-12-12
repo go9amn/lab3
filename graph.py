924b0ea09c7917cedbe7ad9b056b9b2f4f618417
Semen <semenkaurin@gmail.com>
2023-12-12 16:14:06 +0300

    Насрал

--- graph.py
+++ graph.py
@@ -1,12 +1,37 @@
 import json
 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 
 
-def get_data_from_json():
-    with open('outout.json', 'r', encoding='utf-8') as file:
+def get_data_from_json(memory: list, cpu: list) -> None:
+    '''Берет данные из Json и парсит их'''
+    with open('outout.json', 'r') as file:
         data = json.load(file)
-    return data
+        for item in data:
+            memory.append((item[1], item[2]))
+            cpu.append((item[0], item[2]))
 
 
-get_data_from_json()
+def draw_graph(lst: list, name: str) -> None:
+    '''
+    Строит график
+    lst: list - список в котором хранятся данные
+    name: strin - название графика
+    '''
+
+    x, y = [i[0] for i in lst], [i[1] for i in lst]
+    plt.plot(y, x)
+    plt.title(f'{name} graph')
+    plt.xlabel('Time')
+    plt.ylabel(f'{name}')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    memory_time = []
+    cpu_time = []
+    get_data_from_json(memory_time, cpu_time)
+    draw_graph(memory_time, 'Memory')
+    draw_graph(cpu_time, 'CPU')
+
